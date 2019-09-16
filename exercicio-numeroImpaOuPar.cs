fdc4d357b22042afa74deff89f5cc8516f607776
Joas Feitosa <joasfeitosadelimasilva@gmail.com>
2019-09-16 15:55:03 -0300

    Exercicio que imforma numeros impar ou par

---
+++ exercicio-numeroImpaOuPar.cs
@@ -0,0 +1,15 @@
+﻿int n = 0;
+ int r = 0;
+ Console.WriteLine("Determina se o número é par ou impar");
+ Console.Write("Informe um número: ");
+ n = Convert.ToInt32(Console.ReadLine());
+ r = n % 2;
+ if (r == 1)
+ {
+ Console.WriteLine("O número informado é impar");
+ }
+ else
+ {
+ Console.WriteLine("O número informado é par");
+ }
+ Console.ReadKey();
