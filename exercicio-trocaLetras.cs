872b580d063b03e158134f9b97d615308faedc20
Joas Feitosa <joasfeitosadelimasilva@gmail.com>
2019-09-16 22:59:47 -0300

    Exercicio que troca as letras A e a por &

---
+++ exercicio-trocaLetras.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace Exercicio_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            Program p = new Program();
+            string Frase = "Agora eu sou um bom desenvolvedor.";
+ 
+            Console.WriteLine(p.Alterar(Frase));
+ 
+            Console.ReadKey();
+        }
+ 
+        private string Alterar(string Frase)
+        {
+            if (Frase.Trim().Length > 0)
+            {
+                int Total = Frase.Length;
+                int Count = 0;
+ 
+                var StringBuilder = new StringBuilder(Frase);
+ 
+                while (Count < Total)
+                {
+                    if (StringBuilder[Count] == 'o')
+                    {
+                        StringBuilder[Count] = '&';
+                    }
+                    Count++;
+                }
+ 
+                Frase = StringBuilder.ToString();
+ 
+                return Frase;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
