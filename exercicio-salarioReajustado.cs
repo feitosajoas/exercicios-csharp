2e2ccf0e8f95bc2ddd8288d4081933b6472ab45c
Joas Feitosa <joasfeitosadelimasilva@gmail.com>
2019-09-16 22:54:02 -0300

    Exercicio que mostra salario reajustado de 10 pessoas

---
+++ exercicio-salarioReajustado.cs
@@ -0,0 +1,18 @@
+﻿Double salario = 0;
+ Double salarioNovo = 0;
+ Console.WriteLine("Efetua o reajuste salarial");
+ for (int i = 0; i < 3; i++)
+ {
+ Console.Write("Informe o salário: ");
+ salario = Convert.ToDouble(Console.ReadLine());
+ if (salario <= 300)
+ { //50%
+ salarioNovo = salario * 1.50;
+ }
+ else
+ { //30%
+ salarioNovo = salario + ((salario * 30) / 100);
+ }
+ Console.WriteLine("Salário reajustado: " + salarioNovo);
+ }
+ Console.ReadKey();
