f4551edf94d44ce900912cdd0227f5be81bb1ac5
Joas Feitosa <joasfeitosadelimasilva@gmail.com>
2019-09-16 22:52:30 -0300

    Exercicio que mostra pessoas que tem idade 18

---
+++ exercicio-maior-de-idade.cs
@@ -0,0 +1,16 @@
+﻿ int qtde = 0;
+ int idade = 0;
+ Console.WriteLine("Exercício 2 - Verifica maior idade");
+ int i = 0;
+ while (i < 10)
+ {
+ i = i + 1;
+ Console.Write("Informe a idade da pessoa: ");
+ idade = Convert.ToInt32(Console.ReadLine());
+ if (idade >= 18)
+ {
+ qtde = qtde + 1;
+ }
+ }
+ Console.WriteLine("Existem " + qtde + " pessoas com mais de 18 anos");
+ Console.ReadKey();
